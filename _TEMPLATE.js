842fc4222fcc920fa202e5600369992bfb38f623
selfawareghost <fin.basicss@gmail.com>
2026-09-16 16:41:42 +0530

    Delete _TEMPLATE.js

--- _TEMPLATE.js
+++
@@ -1,7 +0,0 @@
-/* Template for a current affairs file. Copy to data/current-affairs/<category-id>.js
-   (e.g. polity.js), then set ready: true for that category in assets/js/config.js. */
-CA.register('polity', {
-  entries: [
-    { month: '2026-05', title: 'Headline of the news item', html: '<p>Your note on it.</p>' }
-  ]
-});
